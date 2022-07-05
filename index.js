aba87d0c9c16f8430da36edea25fa6a81b1553cb
Ivan3 <Ivan3nd3r11@gmail.com>
2022-07-05 13:25:16 +0300

    Instead of parsing the market, JSON is used, real profit is displayed in rubles.

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
-const parse = async () => {
-    const getHTML = async (url) => {
-        const {data} = await axios.get(url);
-        return cheerio.load(data);
-    }
+const getMarketItems = async () => {
+    const {data} = await axios.get('https://market.csgo.com/ajax/i_popularity/all/all/all/1/56/0;500000/all/all/all?sd=desc');
+    return data[0];
+}
 
-    // call first set curency
-
-    // set cookie
-
-    // call again and parse
-    const page = await getHTML('https://market.csgo.com/en');
-    let marketItems = [];
-
-    page('.market-items[id="applications"]').find('a.item ').each((i, el) => {
-        const name = page(el).find('div.name').text().trim();
-        const price = Number(page(el).find('div.price').text().trim());
-        if((!name.includes('Dragon King')) && (!name.includes('StatTrak'))) {
-            marketItems.push({name: name, price: price})
-        }
-    })
-
-    return marketItems
+const getUSDCurrency = async () => {
+    const {data} = await axios.get('https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=ec55a5efa0e734db4506');
+    return data.USD_RUB;
 }
 
 const getMoneyItem = async (itemName) => {
     const {data} = await axios.get(`https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=30&isStore=true&limit=1&maxPrice=10000&minPrice=1&name=${itemName}&offset=0&order=asc&sort=price&withStack=true`);
     if(data?.error === 2) {return data}
-    return data.items[0]
+    return data.items[0];
 }
 
 const compare = async () => {
-    let marketItems = await parse();
-    for(const item of marketItems) {
-        const moneyItem = await getMoneyItem(item.name)
-        if(moneyItem?.price) {
-            const profit = Math.floor(((item.price*0.95)*0.95 - moneyItem.price) * 100) / 100
-            if(profit >= 1) {
-                console.log(item.name, profit)
+    let marketItems = await getMarketItems();
+    const USDCurrency = await getUSDCurrency();
+
+    for(const marketItem of marketItems) {
+        const name = marketItem[8];
+        const price = marketItem[3];
+
+        if ((!name.includes('Dragon King')) && (!name.includes('StatTrak'))) {
+            const moneyItem = await getMoneyItem(name);
+
+            if (moneyItem?.price) {
+                const profit = Math.floor(((price * 0.95) * 0.95 - moneyItem.price * USDCurrency) * 100) / 100;
+                if (profit >= 40) {
+                    console.log(name, profit.toString()+" ₽");
+                }
             }
         }
     }
